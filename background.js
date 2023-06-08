83e7ce0b3aef0a5406c034b55f24f8cd6f23fea4
Guido Preite <43754988+GuidoPreite@users.noreply.github.com>
2023-06-08 09:16:07 +0200

    changed tab property (title to url)

--- background.js
+++ background.js
@@ -3,9 +3,9 @@ chrome.action.onClicked.addListener(function () {
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	if(tab.title.indexOf("https://callbackurl") === 0) {
-		if(tab.title.indexOf("https://callbackurl/#access_token") === 0) {
-			let access_token = tab.title.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
+	if(tab.url.indexOf("https://callbackurl") === 0) {
+		if(tab.url.indexOf("https://callbackurl/#access_token") === 0) {
+			let access_token = tab.url.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
 			chrome.storage.local.set({drb_be_token: access_token}, function() {
 				chrome.tabs.update({url: chrome.runtime.getURL("drb_be_sandbox.htm")});
 			});
